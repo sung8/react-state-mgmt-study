4aeeb0c032c3e59cbc63a5d247d05e8d4a4e8518
sung <heechuw@gmail.com>
2023-07-04 03:45:45 -0400

    useMemo sorting depending on state

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import { useState, useCallback, useMemo } from 'react';
 
 function App() {
   const [numbers] = useState([10, 20, 30]);
-  const inefficientTotal = numbers.reduce((acc, number) => acc + number, 0);
   const total = useMemo(
     () => numbers.reduce((acc, number) => acc + number, 0),
     [numbers]
   );
+
+  const [names] = useState(['John', 'Paul', 'George', 'Ringo']);
+  // const sortInPlaceNames = names.sort();
+  const copyAndSortNames = [...names].sort();
+  const useMemoAndSort = useMemo(() => [...names].sort(), [names]);
   return (
     <div className="App">
-      <div>First Total: {inefficientTotal}</div>
-      <div>Better Total: {total}</div>
+      <div>Total: {total}</div>
+      <div>Names: {names.join(', ')}</div>
+      {/* <div>Sorted In-Place: {sortInPlaceNames.join(', ')}</div> */}
+      <div>Copy and Sort: {copyAndSortNames.join(', ')}</div>
+      <div>Sort Only When Needed (useMemo): {useMemoAndSort.join(', ')}</div>
     </div>
   );
 }
