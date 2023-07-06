27b5e2401ce96ccf5eecd5ceaf1f713b782e4841
sung <heechuw@gmail.com>
2023-07-05 21:35:33 -0400

    useMemo good and bad use case example

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,23 @@ function App() {
   // const sortInPlaceNames = names.sort();
   const copyAndSortNames = [...names].sort();
   const useMemoAndSort = useMemo(() => [...names].sort(), [names]);
+
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const countTotalBad = useMemo(() => count1 + count2, [count1 + count2]);
+  const countTotalBetter = count1 + count2;
+
   return (
     <div className="App">
       <div>Total: {total}</div>
       <div>Names: {names.join(', ')}</div>
       {/* <div>Sorted In-Place: {sortInPlaceNames.join(', ')}</div> */}
-      <div>Copy and Sort: {copyAndSortNames.join(', ')}</div>
+      {/* <div>Copy and Sort: {copyAndSortNames.join(', ')}</div> */}
       <div>Sort Only When Needed (useMemo): {useMemoAndSort.join(', ')}</div>
+      <button onClick={() => setCount1(count1 + 1)}>Count 1: {count1}</button>
+      <button onClick={() => setCount2(count2 + 1)}>Count 2: {count2}</button>
+      <div>Total: {countTotalBetter}</div>
     </div>
   );
 }
