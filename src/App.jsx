aa9bd62bbe29fcfccbbeaaee76da408e6b47044e
sung <heechuw@gmail.com>
2023-07-09 00:06:37 -0400

    useEffect displaying associated data with buttons and clicks

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,25 @@ function App() {
       .then((data) => setNames(data));
   }, []);
 
+  const [selectedName, setSelectedName] = useState(null);
+
+  const [selectedNameDetails, setSelectedNameDetails] = useState(null);
+
+  useEffect(() => {
+    fetch(`/${selectedName}.json`)
+      .then((res) => res.json())
+      .then((data) => setSelectedNameDetails(data));
+  }, [selectedName]);
+
   return (
     <div className="App">
       <div>Names: {names.join(', ')}</div>
+      <div>
+        {names.map((name) => (
+          <button onClick={() => setSelectedName(name)}>{name}</button>
+        ))}
+      </div>
+      <div>{JSON.stringify(selectedNameDetails)}</div>
     </div>
   );
 }
