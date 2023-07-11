9eea5d99a648089c4efc146e6ef467af19715d8f
sung <heechuw@gmail.com>
2023-07-10 23:05:01 -0400

    useRef get reference to HTML element

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,15 @@
-import { useState, useEffect } from 'react';
-
-const Stopwatch = () => {
-  const [time, setTime] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime((t) => {
-        console.log(t);
-        return t + 1;
-      });
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-
-  return <div>Time: {time}</div>;
-};
+import { useState, useRef, useEffect } from 'react';
 
 function App() {
-  const [names, setNames] = useState([]);
+  const inputRef = useRef(null);
 
   useEffect(() => {
-    fetch('/names.json')
-      .then((res) => res.json())
-      .then((data) => setNames(data));
+    inputRef.current.focus();
   }, []);
-
-  const [selectedNameDetails, setSelectedNameDetails] = useState(null);
-
-  const onSelectedName = (name) => {
-    fetch(`/${name}.json`)
-      .then((res) => res.json())
-      .then((data) => setSelectedNameDetails(data));
-  };
 
   return (
     <div className="App">
-      <Stopwatch />
-      <div>Names: {names.join(', ')}</div>
-      <div>
-        {names.map((name) => (
-          <button onClick={() => onSelectedName(name)}>{name}</button>
-        ))}
-      </div>
-      <div>{JSON.stringify(selectedNameDetails)}</div>
+      <input type="text" ref={inputRef} />
     </div>
   );
 }
