fe74bac1d4f13846f270e40d54f79d7f4294cdd1
sung <heechuw@gmail.com>
2023-07-11 01:12:26 -0400

    code finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,7 @@
-import { useRef, useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function App() {
-  const inputRef = useRef(null);
-
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
-
-  const idRef = useRef(1);
-  const [names, setNames] = useState([
-    { id: idRef.current++, name: 'John' },
-    { id: idRef.current++, name: 'Jane' },
-  ]);
-
-  const onAddName = () => {
-    setNames([
-      ...names,
-      {
-        id: idRef.current++,
-        name: inputRef.current.value,
-      },
-    ]);
-    inputRef.current.value = '';
-  };
-
-  return (
-    <div>
-      <div>
-        {names.map((name) => (
-          <div key={name.name}>
-            {name.id} - {name.name}
-          </div>
-        ))}
-      </div>
-      <input type="text" ref={inputRef} />
-      <button onClick={onAddName}>Add Name</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
